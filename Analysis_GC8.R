4003103a03e321d2de1b7245fdd9b835bb785dfd
Bruno Alves De Carvalho <balvesdecarvalho@gmail.com>
2024-12-02 21:44:51 +0100

    Add work

--- Analysis_GC8.R
+++ Analysis_GC8.R
@@ -13,10 +13,55 @@ library(googlesheets4)
 sheet_url <- 
   "https://docs.google.com/spreadsheets/d/1tc4cgr_uA36VCEW33GbZVTaPehKALThdjSd2QzBSYgM/edit?gid=406461073#gid=406461073"
 
-# Load OECD data
-OECD_Data <- 
-  rename(
-    read_sheet(
-      sheet_url, sheet = "ODA Disbursements 2001-2023"
-      )[c("Donor", "TIME_PERIOD", "OBS_VALUE")], c("donor" = "Donor", "year" = "TIME_PERIOD", "oda_spent" = "OBS_VALUE")
+# Create function to load data
+my_sheet_names <- 
+  c(
+    "OECD" = "ODA Disbursements 2001-2023", 
+    "DGD" ="Other Replenishments", 
+    "TGF" = "0-7 Replenishments", 
+    "IMF" = "Macroeconomics 2000-2024"
     )
+
+load_data <- 
+  function(x) {
+    read_sheet(
+      sheet_url, sheet = my_sheet_names[[x]]
+    )
+  }
+
+# Load data
+list_data <- tibble(
+  source = names(my_sheet_names),
+  data = vector("list", length(my_sheet_names))
+)
+
+for (i in seq_along(my_sheet_names)) {
+  list_data$data[[i]] <- load_data(i) 
+}
+
+# Select and rename columns from OECD data
+list_data$data[[1]] <- 
+  list_data %>% 
+  pluck(2, 1) %>% 
+  select(
+    "Donor", 
+    "TIME_PERIOD", 
+    "OBS_VALUE"
+    ) %>% 
+  rename(
+    "donor_name" = "Donor", 
+    "year" = "TIME_PERIOD", 
+    "oda_spent" = "OBS_VALUE"
+    )
+
+# Gather CGD data into a long format
+list_data$data[[2]] <-
+  list_data$data[[2]] %>% 
+  pivot_longer(
+    cols = matches("\\d{4}$"), 
+    names_to = "org_year", 
+    values_to = "money")
+
+## to-do: link each year to a Grant Cycle; look for pledge realization by year
+
+
