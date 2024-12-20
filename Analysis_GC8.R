78aa194df5e128cabf7bc428bd0b6547af14295f
Bruno Alves De Carvalho <balvesdecarvalho@gmail.com>
2024-12-20 11:56:23 +0100

    Commit work on data analysis

--- Analysis_GC8.R
+++ Analysis_GC8.R
@@ -60,7 +60,66 @@ list_data$data[[2]] <-
   pivot_longer(
     cols = matches("\\d{4}$"), 
     names_to = "org_year", 
-    values_to = "money")
+    values_to = "money") %>% 
+  separate(org_year, into = c("org", "year"), sep = "_") %>% 
+  mutate(
+    year = as.numeric(year), 
+    "grant_cycle" = 
+      ifelse(between(year, 2000, 2002), "GC0", 
+             ifelse(between(year, 2003, 2005), "GC1", 
+                    ifelse(between(year, 2006, 2008), "GC2", 
+                           ifelse(between(year, 2009, 2011), "GC3", 
+                                  ifelse(between(year, 2012, 2014), "GC4", 
+                                         ifelse(between(year, 2015, 2017), "GC5", 
+                                                ifelse(between(year, 2018, 2020), "GC6", 
+                                                       ifelse(between(year, 2021, 2023), "GC7", 
+                                                              "GC8")
+                                                )
+                                         )
+                                  )
+                           )
+                    )
+             )
+      )
+  ) %>% 
+  pivot_wider(names_from = org, values_from = money) %>% 
+  group_by(donor_name, grant_cycle) %>% 
+  summarise(
+    GAVI = sum(GAVI, na.rm = T), 
+    ADF = sum(ADF, na.rm = T), 
+    IFAD = sum(IFAD, na.rm = T), 
+    IDA = sum(IDA, na.rm = T), 
+    GCF = sum(GCF, na.rm = T), 
+    PF = sum(PF, na.rm = T), 
+    LDF = sum(LDF, na.rm = T), 
+    GEF = sum(GEF, na.rm = T), 
+    GPE = sum(GPE, na.rm = T), 
+    AfDf = sum(AfDf, na.rm = T), 
+    CEPI = sum(CEPI, na.rm = T)
+  )
+
+# Create Grant Cycle column
+list_data$data[[3]] <- 
+  list_data %>% 
+  pluck(2,3) %>% 
+  mutate(
+    grant_cycle = ifelse(year == 2001, "GC0", 
+                         ifelse(year == 2005, "GC1", 
+                                ifelse(year == 2007, "GC2", 
+                                       ifelse(year == 2010, "GC3", 
+                                              ifelse(year == 2013, "GC4", 
+                                                     ifelse(year == 2016, "GC5", 
+                                                            ifelse(year == 2019, "GC6", 
+                                                                   ifelse(year == 2022, "GC7", "GC8")
+                                                                   )
+                                                            )
+                                                     )
+                                              )
+                                       )
+                                )
+                         )
+    )
+
 
 ## to-do: link each year to a Grant Cycle; look for pledge realization by year
 
