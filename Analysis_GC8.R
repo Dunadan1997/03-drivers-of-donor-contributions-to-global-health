d201f878eac1c25619671762fd36a3e1bf4352c6
Bruno Alves De Carvalho <balvesdecarvalho@gmail.com>
2025-01-26 16:19:17 +0100

    Commit work in progress

--- Analysis_GC8.R
+++ Analysis_GC8.R
@@ -11,49 +11,35 @@ library(googlesheets4)
 library(slider)
 library(stringi)
 
-# Link to Google Sheets
-sheet_url <- 
-  "https://docs.google.com/spreadsheets/d/1tc4cgr_uA36VCEW33GbZVTaPehKALThdjSd2QzBSYgM/edit?gid=406461073#gid=406461073"
 
-# Create functions to load data
-my_sheet_names <- 
-  c(
-    "OECD" = "ODA Disbursements 1997-2023", 
-    "CGD" ="Other Replenishments", 
-    "TGF" = "0-7 Replenishments" 
-    )
+# Path to Data Warehouse
+path_to_data_warehouse <- 
+  "/Users/brunoalvesdecarvalho/Desktop/DataWarehouse_20231015_ve01/TGF"
 
-load_data <- 
-  function(x) {
-    read_sheet(
-      sheet_url, sheet = my_sheet_names[[x]]
-    )
+# Copy the URLs of each dataset
+urls <- 
+  list.files(path_to_data_warehouse)
+
+# Store URLs in a table
+urls_table <- tibble(
+  source = str_extract_all(urls, "^[^_]+", simplify = TRUE), 
+  url = paste0(path_to_data_warehouse, sep = "/", urls)
+  ) %>%
+  filter(!str_detect(source, "^\\d"))
+
+# Create a function to load data
+load_data <- function(url_link_index) {
+    read_csv(urls_table$url[[url_link_index]])
   }
 
-load_data <- 
-  function(location, source_name = NULL, url_link = NULL, sheet_name = NULL) {
-    if (location == "Google Drive") {
-      read_sheet(
-        sheet_url, sheet = my_sheet_names[[sheet_name]]
-      ) 
-    } else if (location == "Desktop") {
-      list_data %>% 
-        add_row(
-          source = source_name,
-          data = list(as_tibble(read_csv(url_link)))
-          )
-    }
-  }
-
-
-# Load OEDC, CGD, and TGF data
+# Load all data
 list_data <- tibble(
-  source = names(my_sheet_names),
-  data = vector("list", length(my_sheet_names))
+  source = urls_table$source,
+  data = vector("list", length(urls_table$url))
 )
 
-for (i in seq_along(my_sheet_names)) {
-  list_data$data[[i]] <- load_data("Google Drive", sheet_name = i) 
+for (i in seq_along(urls_table$url)) {
+  list_data$data[[i]] <- load_data(i) %>% tibble()
 }
 
 # create a copy of the loaded data
@@ -64,7 +50,7 @@ copy_list_data <-
 list_data$data[[which(list_data$source == "OECD")]] <- 
   list_data %>% 
   filter(source == "OECD") %>% 
-  pluck(2) %>%
+  pluck(2,1) %>%
   select(
     "Donor", 
     "TIME_PERIOD", 
@@ -86,10 +72,10 @@ list_data$data[[which(list_data$source == "OECD")]] <-
   ungroup()
 
 # Gather CGD data by name of organization and replenishment year
-list_data$data[[which(list_data$source == "CDG")]] <-
+list_data$data[[which(list_data$source == "CGD")]] <-
   list_data %>% 
-  filter(source == "CDG") %>% 
-  pluck(2) %>% 
+  filter(source == "CGD") %>% 
+  pluck(2,1) %>% 
   pivot_longer(
     cols = matches("\\d{4}$"), 
     names_to = "org_year", 
@@ -120,19 +106,19 @@ list_data$data[[which(list_data$source == "CDG")]] <-
 # Create table summarizing the number of replenishments per Grant Cycle
 tab_n_rplnshmnt <-
   list_data %>% 
-  filter(source == "CDG") %>% 
-  pluck(2) %>%
+  filter(source == "CGD") %>% 
+  pluck(2,1) %>%
   select(-donor_name) %>% 
   group_by(org, year, grant_cycle) %>% 
   summarise(sum = sum(money, na.rm = T)) %>% 
   group_by(grant_cycle) %>% 
   summarise(n_rplnshmnt = n())
 
-# Spread CDG data by MDB and Health Fun
-list_data$data[[which(list_data$source == "CDG")]] <-
+# Spread CGD data by MDB and Health Fun
+list_data$data[[which(list_data$source == "CGD")]] <-
   list_data %>% 
-  filter(source == "CDG") %>% 
-  pluck(2) %>%
+  filter(source == "CGD") %>% 
+  pluck(2,1) %>%
   pivot_wider(names_from = org, values_from = money) %>% 
   group_by(donor_name, grant_cycle) %>% 
   summarise(
@@ -156,7 +142,7 @@ list_data$data[[which(list_data$source == "CDG")]] <-
 list_data$data[[which(list_data$source == "TGF")]] <- 
   list_data %>% 
   filter(source == "TGF") %>% 
-  pluck(2) %>% 
+  pluck(2,1) %>% 
   mutate(
     grant_cycle = ifelse(year == 2001, "GC0", 
                          ifelse(year == 2005, "GC1", 
@@ -175,26 +161,24 @@ list_data$data[[which(list_data$source == "TGF")]] <-
                          )
     )
 
-
-# Load IMF data
-list_data <- load_data(
-  location = "Desktop", 
-  source_name = "IMF", 
-  url_link = "/Users/brunoalvesdecarvalho/Desktop/Research/IMF/dataset_2024-12-22T16_56_47.354302444Z_DEFAULT_INTEGRATION_IMF.FAD_FM_2.0.0.csv"
-  )
-
 # Select relevant variables from IMF data
+IMF_colnames <- 
+  list_data %>% 
+  filter(source == "IMF") %>% 
+  pluck(2,1) %>% 
+  colnames()
+
 list_data$data[[which(list_data$source == "IMF")]] <-
   list_data %>% 
   filter(source == "IMF") %>% 
-  pluck(2) %>% 
+  pluck(2,1) %>% 
   select(
     donor_name = COUNTRY.Name, 
     fiscal_indicator = INDICATOR.Name, 
     starts_with("19"), 
     starts_with("20")) %>% 
   pivot_longer(
-    cols = na.omit(str_extract(colnames(IMF_data), "\\d+")), 
+    cols = na.omit(str_extract(IMF_colnames, "\\d+")), 
     names_to = "year", values_to = "obs_value") %>% 
   mutate(
     donor_name = str_extract(donor_name, "^[^,]+")
@@ -226,18 +210,11 @@ list_data$data[[which(list_data$source == "IMF")]] <-
   rename_with(~ str_replace_all(.x, "_prctgdp_", "_")) %>% 
   ungroup()
 
-# Load PAGED data
-list_data <- load_data(
-  location = "Desktop", 
-  source_name = "PAGED", 
-  url_link = "/Users/brunoalvesdecarvalho/Desktop/Research/Party_Government/PAGED-WECEE.csv"
-)
-
 # Select relevant variables from PAGED data
 list_data$data[[which(list_data$source == "PAGED")]] <- 
   list_data %>% 
   filter(source == "PAGED") %>% 
-  pluck(2) %>%  
+  pluck(2,1) %>%  
   select(
     country_name, 
     year = year_in, 
@@ -251,7 +228,7 @@ list_data$data[[which(list_data$source == "PAGED")]] <-
     party_name_short = stri_trans_general(party_name_short, "latin-ascii"),
     country_name = ifelse(country_name == "Czechia", "Czech Republic", country_name),
     elecdate = year(dmy(elecdate)),
-# Create variable recording the year the left-right placement was made to link data in PAGED and CHESS
+# Create variable recording the year the Expert Survey (CHES) was conducted. This will help link data in PAGED and CHESS
     yr_rating_reported = 
       ifelse(year_out < 2000, 1999, 
              ifelse(year < 2005, 2002, 
@@ -280,7 +257,7 @@ paged_data_2024 <-
     year_out = NA,
     party_name_short = c("FPO", "VLD", "GERB", "HDZ", "S", "S", "ER", "KOK", "LREM", "SPD", "ND", "Fidesz", "LG", "SDA", "FF", "FF", "FdI", "JV",
                          "LSDP", "VVD", "PVV", "A", "PO", "PS", "PS", "PSD", "PSD", "Smer-SD", "GS", "PSOE", "PSOE", "M", "Con", "Lab"),
-    party_id_2 = c(1303, 107, 2010, 3101, 201, 201, 2203, 1402, 626, 302, 402, 2302, 4502, 4505, 701, 701, 844, 2412,
+    party_id_ches = c(1303, 107, 2010, 3101, 201, 201, 2203, 1402, 626, 302, 402, 2302, 4502, 4505, 701, 701, 844, 2412,
                  2501, 1003, 1017, 3501, 2603, 1205, 1205, 1206, 2701, 2803, 2916, 501, 501, 1605, 1101, 1102),
     elecdate = c(2024, 2024, 2024, 2024, 2019, 2022, 2023, 2023, 2024, 2021, 2023, 2022, 2021, 2024, 2020, 2024, 2022, 2022,
                  2024, 2021, 2023, 2021, 2023, 2019, 2022, 2024, 2024, 2023, 2022, 2019, 2023, 2022, 2019, 2024),
@@ -292,44 +269,29 @@ paged_data_2024 <-
 list_data$data[[which(list_data$source == "PAGED")]] <-
   list_data %>% 
   filter(source == "PAGED") %>% 
-  pluck(2) %>% 
-  bind_rows(paged_data_2024) %>% 
+  pluck(2,1) %>% 
+  bind_rows(paged_data_2024 %>% select(-party_id_ches)) %>% 
 # Correct minor issues with cabinet periods
   mutate(
     yr_rating_reported = 
       ifelse(country_name == "Czech Republic" & year == 2021, 2024, 
              ifelse(country_name == "Italy" & year == 2018, 2019, yr_rating_reported)))
 
-# Load CHESS data from 1999 to 2019
-list_data <- 
-  load_data(
-    location = "Desktop", 
-    source_name = "CHESS", 
-    url_link = "/Users/brunoalvesdecarvalho/Desktop/Research/Party_Orientation/chess/1999-2019_CHES_dataset_means(v3).csv"
-  )
-
-# Load CHESS data from 2024
-list_data <- 
-  load_data(
-    location = "Desktop", 
-    source_name = "CHESS_24", 
-    url_link = "/Users/brunoalvesdecarvalho/Desktop/Research/Party_Orientation/chess/CHES_Ukraine_March_2024.csv"
-  )
 
 # Select relevant variables
-list_data$data[[which(list_data$source == "CHESS_24")]] <-
+list_data$data[[which(list_data$source == "CHES24")]] <-
   list_data %>%
-  filter(source == "CHESS_24") %>% 
-  pluck(2) %>% 
-  select(country_name = country, party_name_short = party, lrecon, party_id) %>%
+  filter(source == "CHES24") %>% 
+  pluck(2,1) %>% 
+  select(country_name = country, party_name_short = party, lrecon, party_id_ches = party_id) %>%
 # Clean political party abbreviations
   mutate(yr_rating_reported = 2024,
          party_name_short = stri_trans_general(party_name_short, "latin-ascii"),
-         party_name_short = ifelse(party_id == 2412, "JV", party_name_short)
+         party_name_short = ifelse(party_id_ches == 2412, "JV", party_name_short)
          ) 
 
 # Create tibble attributing countries names to country IDs in the CHESS data
-chess_country_names <- 
+ches_country_names <- 
   tibble(
     country_id = c(1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 16, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 37, 38, 40),
     country_name = c("Belgium", "Denmark", "Germany", "Greece", "Spain", "France", "Ireland", "Italy", "Netherlands", "United Kingdom",
@@ -338,28 +300,30 @@ chess_country_names <-
     )
 
 # Select relevant variables from CHESS data
-list_data$data[[which(list_data$source == "CHESS")]] <-
+list_data$data[[which(list_data$source == "CHES")]] <-
   list_data %>% 
-  filter(source == "CHESS")
-  pluck(2) %>% 
-  select(country_id = country, yr_rating_reported = year, party_name_short = party, lrgen, lrecon, party_id) %>% 
+  filter(source == "CHES") %>% 
+  pluck(2,1) %>% 
+  select(country_id = country, yr_rating_reported = year, party_name_short = party, lrgen, lrecon, party_id_ches = party_id) %>% 
 # Add country names
-  left_join(chess_country_names, by = "country_id") %>% 
+  left_join(ches_country_names, by = "country_id") %>% 
 # Add 2024 data
   bind_rows(
     list_data %>%
-      filter(source == "CHESS_24") %>% 
-      pluck(2)) %>% 
+      filter(source == "CHES24") %>% 
+      pluck(2,1)) %>% 
   select(country_name, everything(), -country_id)
 
 # Remove CHESS_24 as 2024 data was combined with the rest of the CHESS data
 list_data <-
   list_data %>% 
-  filter(source != "CHESS_24")
+  filter(source != "CHES24")
 
 # Create a table of all political parties in PAGED
 parties_list <- 
-  paged_data_test %>% 
+  list_data %>%
+  filter(source == "PAGED") %>% 
+  pluck(2,1) %>% 
   filter(year > 1998 & !is.na(party_name_short)) %>% 
   group_by(country_name, party_name_short) %>% 
   summarise(n = n())
@@ -369,9 +333,11 @@ countries_to_check <-
   group_by(country_name) %>% 
   summarise(n = n()) %>% 
   pull(country_name)
-# Create a table of all ideological ratings in CHESS
+# Create a table of all political parties with an ideological rating in CHES
 parties_ratings <- 
-  chess_data_test %>% 
+  list_data %>%
+  filter(source == "CHES") %>% 
+  pluck(2,1) %>%  
   filter(!is.na(party_name_short)) %>% 
   group_by(country_name, party_name_short) %>% 
   summarise(n = n())
@@ -431,7 +397,7 @@ list_new_party_names <-
   mutate(
     adjust_dataset = ifelse(
       party_name_short == new_names, 
-      "CHESS", "PAGED"
+      "CHES", "PAGED"
     )
   )
 
@@ -448,9 +414,11 @@ list_new_party_names_PAGED <-
   )
 
 # Correct party names in the PAGED data
-paged_data_test <- reduce(
+list_data$data[[which(list_data$source == "PAGED")]] <- reduce(
   seq_len(nrow(list_new_party_names_PAGED)),
-  .init = paged_data_test,
+  .init =  list_data %>%
+    filter(source == "PAGED") %>% 
+    pluck(2,1),
   .f = function(data, i) {
     data %>% 
       mutate(
@@ -464,15 +432,15 @@ paged_data_test <- reduce(
   }
 )
 
-# Create a table of the party names to change in the CHESS data
-list_new_party_names_CHESS <-
+# Create a table of the party names to change in the CHES data
+list_new_party_names_CHES <-
   list_new_party_names %>% 
-  filter(adjust_dataset == "CHESS") %>% 
+  filter(adjust_dataset == "CHES") %>% 
   filter(!party_name_short %in% c("IL", "ResP", "Independent"))
 
-list_new_party_names_CHESS <-
-  list_new_party_names_CHESS %>% 
-  bind_rows(list_new_party_names_CHESS %>% slice(10)) %>% 
+list_new_party_names_CHES <-
+  list_new_party_names_CHES %>% 
+  bind_rows(list_new_party_names_CHES %>% slice(10)) %>% 
   mutate(
     party_name_short = c(
       "Sj", "Graen", "F", "Sam", 
@@ -488,50 +456,65 @@ list_new_party_names_CHESS <-
     )
   )
 
-# Correct party names in the CHESS data
-chess_data_test <- reduce(
-  seq_len(nrow(list_new_party_names_CHESS)),
-  .init = chess_data_test,
+# Correct party names in the CHES data
+list_data$data[[which(list_data$source == "CHES")]] <- reduce(
+  seq_len(nrow(list_new_party_names_CHES)),
+  .init = list_data %>%
+    filter(source == "CHES") %>% 
+    pluck(2,1),
   .f = function(data, i) {
     data %>% 
       mutate(
         party_name_short = ifelse(
-          country_name == list_new_party_names_CHESS[[i, 1]] & 
-            party_name_short == list_new_party_names_CHESS[[i, 2]],
-          list_new_party_names_CHESS[[i, 5]],
+          country_name == list_new_party_names_CHES[[i, 1]] & 
+            party_name_short == list_new_party_names_CHES[[i, 2]],
+          list_new_party_names_CHES[[i, 5]],
           party_name_short
         )
       )
   }
 )
 
-paged_data_test2 <-
-  paged_data_test %>% 
-  filter(
-    !is.na(party_name_short)
-  ) %>% 
+# Summarize the ID of each party in CHES to prepare join with PAGED
+ches_party_ids <-
+  list_data %>% 
+    filter(source == "CHES") %>% 
+    pluck(2,1) %>% 
+    group_by(country_name, party_name_short, party_id_ches) %>%
+    count() %>% 
+    select(-n) %>% 
+    ungroup()
+
+# Join PAGED and CHES data into one set
+PAGED_CHES <-
+  list_data %>% 
+  filter(source == "PAGED") %>% 
+  pluck(2,1) %>% 
+  filter(!is.na(party_name_short)) %>% 
+# Join party IDs from CHES to PAGED using party names
+  left_join(ches_party_ids, by = c("country_name", "party_name_short")) %>%
+# Join ideological ratings from CHES to PAGED using party IDs
   left_join(
-    chess_data_test %>% 
-      group_by(country_name, party_name_short, party_id) %>%
-      summarise(n = n()) %>% 
-      select(-n) %>% 
-      ungroup(), 
-    by = c("country_name", "party_name_short")) %>% 
-  left_join(
-    chess_data_test %>% 
+    list_data %>%
+      filter(source == "CHES") %>% 
+      pluck(2,1) %>% 
       select(-country_name, -party_name_short), 
-    by = c("yr_rating_reported", "party_id")) %>% 
-  left_join(
-    paged_data_test %>% 
-      group_by(elecdate) %>% 
-      summarise(n_elections = n()), 
-    by = "elecdate") %>% 
-# Bring last observation forward (and then backward) if value is missing
-  group_by(party_id) %>% 
+    by = c("yr_rating_reported", "party_id_ches")) %>% 
+# Bring last rating forward (and then backward) if rating is missing
+  group_by(party_id_ches) %>% 
   fill(lrgen, .direction = "downup") %>% 
   fill(lrecon, .direction = "downup") %>% 
   ungroup()
 
+# Add PAGED_CHES dataset with the rest of the data
+list_data <-
+  list_data %>% 
+  add_row(
+    source = "PAGED_CHES",
+    data = list(PAGED_CHES)
+  )
+
+  
 # Bring last observation backward if value is missing
 chess_ice_nor_2024 <- 
   chess_data_test %>% 
@@ -549,10 +532,6 @@ paged_data_test2 <-
 # ratings we still need to fix
 paged_data_test2 %>% filter(year_in > 1999 & is.na(lrecon))
 
-# Load MPD data
-mpd_data_long <- 
-  read_csv("/Users/brunoalvesdecarvalho/Desktop/Research/Party_Orientation/ManifestoPD/MPDataset_MPDS2024a.csv")
-
 # Select and rename relevant variables from MPD data
 mpd_data_short <-
   mpd_data_long %>% 
@@ -568,10 +547,6 @@ mpd_data_short <-
 # Re-scale the Left-Right scale from -100-100 to 0-1 
     lrgen = (rile + 100)/20
     )
-
-# Load ParlGov data
-parlgov_data_long <- 
-  read_csv("/Users/brunoalvesdecarvalho/Desktop/Research/Party_Government/parlgov/view_cabinet.csv")
 
 # Select and rename relevant variables from ParlGov data
 parlgov_data_short <-
@@ -690,8 +665,8 @@ eurocomm_data <-
   mutate(avg_rating = mean(c(lrgen, lrecon), na.rm = T))
 
 
-# to-do 1: manually complete governments for the United States, South Korea, and the European Commission (for the EC, construct the L-R based on EU member parties)
 # to-do 2: assign ratings to grant cycles based on proximity
+# to-do 3: calculate n_elections for each year
 
 # research longitudinal data on ideological placement for other regions (non-EU)
 
